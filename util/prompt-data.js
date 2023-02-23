46bd0884071e59e5f1feffe75956f0d8c191eba8
artificialprinter <126105578+artificialprinter@users.noreply.github.com>
2023-02-23 03:44:03 -0800

    Update prompt-data.js

--- util/prompt-data.js
+++ util/prompt-data.js
@@ -1,32 +1,326 @@
 export default [
     {
         'key': 'extend-text:1',
-        'value': 'Inspired by childhood memories, reminiscent of classic toys, nostalgic charm, playful design, timeless appeal, reminiscent of simpler times, heartwarming sentiment, emotional connection, evocative of innocence, collected treasure',
+        'value': 'highly detailed, by Dustin Nguyen, Akihiko Yoshida, Greg Tocchini, Greg Rutkowski, Cliff Chiang, 4k resolution, Dishonored inspired, bravely default inspired, vibrant but dreary red, black and white color scheme, epic extreme long shot, dark mood and strong backlighting, volumetric lights, smoke volutes, artstation HQ, unreal engine, octane renderer, HQ, 8K',
         'created': '2023-02-02T14:22:43.000Z',
         'modified': '2023-02-02T14:22:43.000Z'
     },
     {
         'key': 'extend-text:2',
-        'value': 'Hand-painted details, intricate patterns, delicate features, lifelike appearance, realistic texture, high quality material, unique design, artistic expression, vibrant colors, collectible piece',
+        'value': '3d render, unreal engine, ultradetailed, trending on artstation, devianart, cgsociety, concept art, 8k, render',
         'created': '2023-02-02T14:23:33.000Z',
         'modified': '2023-02-02T14:23:33.000Z'
     },
     {
         'key': 'extend-text:3',
-        'value': 'Artist collaborations, limited edition pieces, exclusive designs, signed by the artist, one-of-a-kind creations, premium quality, collectible value, highly sought after, prestigious ownership, rare finds',
+        'value': 'pastel, anime style, clean sharp digital art, environment concept art, by rossdraws, ghibli, breath of the wild, greg rutkowski',
         'created': '2023-02-02T14:23:53.000Z',
         'modified': '2023-02-02T14:23:53.000Z'
     },
     {
         'key': 'extend-text:4',
-        'value': 'Inspired by pop culture, iconic characters, fan-favorite designs, beloved franchises, popular fandoms, highly recognizable, culturally significant, trendsetting style, collectible merchandise, pop culture phenomena',
+        'value': ' by tim okamura, victor nizovtsev, noah bradley, trending on artstation, 8k, masterpiece, graffiti paint, fine detail, full of color, intricate detail, illustration',
         'created': '2023-02-02T14:24:16.000Z',
         'modified': '2023-02-02T14:24:16.000Z'
     },
     {
         'key': 'extend-text:5',
-        'value': ', detailed, intricate, full of color, cinematic lighting, hyperrealistic, focused, extreme details, unreal engine 5, cinematic, masterpiece',
-        'created': '2023-02-02T14:24:16.000Z',
-        'modified': '2023-02-02T14:24:16.000Z'
+        'value': 'detailed, intricate, full of color, cinematic lighting, hyperrealistic, focused, extreme details, unreal engine 5, cinematic, masterpiece',
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:6',
+        'value': 'centered , detailed, anime style, key visual ,intricate detail, highly detailed, breathtaking, vibrant, panoramic, cinematic, Carne Griffiths, Conrad Roset, Makoto Shinkai',        
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:7',
+        'value': 'Pixar style,4k, 8k, unreal engine, octane render photorealistic by cosmicwonder, hdr, photography by cosmicwonder, high definition, symmetrical face, volumetric lighting, photo, octane render, 24mm, 4k, 24mm, DSLR, high quality, 60 fps, ultra realistic',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:8',
+        'value': 'centered, not cropped, painted, fantasy, intricate, elegant, highly detailed, digital painting, artstation, concept art, smooth, sharp focus, illustration, art by gaston bussiere and alphonse mucha',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:9',
+        'value': 'highly detailed marble and jade sculpture, volumetric fog, Hyperrealism, breathtaking, ultra realistic, unreal engine, ultra detailed, cyber background, Hyperrealism, cinematic lighting, highly detailed, breathtaking , photography, stunning environment, wide-angle',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:10',
+        'value': 'pen and ink, intricate line drawings, by craig mullins, ruan jia, kentaro miura, greg rutkowski, loundraw',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:11',
+        'value': 'totem aztek greeble tribal style fanart ornate fantasy heartstone ankama gta5 cover style official behance hd artstation by Jesper Ejsing, by RHADS, Makoto Shinkai and Lois van baarle, ilya kuvshinov, rossdraws totem color pastel vibrant radiating a glowing aura intricate, elegant, sharp focus, illustration, digital painting, concept art, matte, art by WLOP and Artgerm and Greg Rutkowski and Alphonse Mucha, masterpiece',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:12',
+        'value': 'poster, on hill, centered, key visual, intricate, highly detailed, breathtaking, precise lineart, vibrant, panoramic, cinematic, Carne Griffiths,Conrad Roset',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:13',
+        'value': 'painting by Gustav Klimt, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:14',
+        'value': 'painting by Pablo Picasso, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:15',
+        'value': 'painting by Banksy, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:16',
+        'value': 'painting by Arthur Adams, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:17',
+        'value': 'painting by Eileen Agar, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:17',
+        'value': 'painting by Yaacov Agam, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:18',
+        'value': 'painting by Jacques-Laurent Agasse, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:19',
+        'value': 'painting by Ivan Aivazovsky, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:20',
+        'value': 'painting by David Aja, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:21',
+        'value': 'painting by Rafael Albuquerque, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:22',
+        'value': 'painting by Chiho Aoshima, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:23',
+        'value': 'painting by Hirohiko Araki, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:24',
+        'value': 'painting by Alexander Archipenko, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:25',
+        'value': 'painting by El Anatsui, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:26',
+        'value': 'painting by Karol Bak, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:27',
+        'value': 'painting by Christopher Balaskas, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:28',
+        'value': 'painting by Carl Barks, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:29',
+        'value': 'painting by Cicely Mary Barker, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:30',
+        'value': 'art by Jean-Michel Basquiat, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:31',
+        'value': 'art by Romare Bearden, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:32',
+        'value': 'art by Aubrey Beardsley, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:33',
+        'value': 'art by Ivan Bilibin, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:34',
+        'value': 'art by Xu Bing, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:35',
+        'value': 'art by Robert Bissell, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:36',
+        'value': 'art by Anna Bocek, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:37',
+        'value': 'art by Richard Parkes Bonington, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:38',
+        'value': 'art by Franklin Booth, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:39',
+        'value': 'art by Susan Seddon Boulet, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:40',
+        'value': 'art by Frank Bramley, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:41',
+        'value': 'art by Georges Braque, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:42',
+        'value': 'art by Mark Briscoe, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:43',
+        'value': 'art by Stasia Burrington, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:44',
+        'value': 'art by Pascale Campion, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:45',
+        'value': 'art by Camilla dErrico, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
+    }
+    ,
+    {
+        'key': 'extend-text:46',
+        'value': 'art by Michael DeForge, intricate details, fine details, good proportions, highly detailed, breathtaking, vibrant, colorful, ultra detailed',           
+        'created': '2023-02-23T14:24:16.000Z',
+        'modified': '2023-02-23T14:24:16.000Z'
     }
 ];
